408d866c6268b64c6aa633b73d691f9220817292
Vishal Salgond <vnsalgond@gmail.com>
2020-11-25 17:39:55 +0530

    Added recommendation

--- find_college.php
+++ find_college.php
@@ -23,6 +23,185 @@
         include 'connect.php'; 
     ?>
 
+    <div class="container my-5">
+        <h1>Colleges recommended for you</h1>
+
+        <h3 class="mt-5">Based on JEE Advance</h3>
+        <div class="row row-cols-1 row-cols-md-2 pt-3">
+            <?php 
+                $query = "SELECT * FROM college";
+                $count = 0;
+                if ($result = $conn->query($query)) {
+                    
+                    while ($row = $result->fetch_assoc()) {
+                        if($row['mode_of_admission'] == 'JEE Advance') {
+                            $cs = $row['computer_engineering']; 
+                            $mech = $row['mechanical_engineering']; 
+                            $etc = $row['electrical_engineering']; 
+                            $ece = $row['electronics_engineering']; 
+                            $che = $row['chemical_engineering'];
+
+                            if(max($cs, $mech, $etc, $ece, $che) >= $_SESSION['jee_adv']) {
+                                $count += 1;
+                                echo '<div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                                    <div class="card-header">Recommended</div>
+                                    <div class="card-body">';
+                                        echo '<h5 class="card-title">'.$row['name'].'</h5>';
+                                        echo '<h6 class="card-title mt-3">Available branches</h6>';
+                                        echo '<ul>';
+                                            if($cs >= $_SESSION['jee_adv']) {
+                                                echo '<li>Computer Engineering</li>';
+                                            }
+                                            if($mech >= $_SESSION['jee_adv']) {
+                                                echo '<li>Mechanical Engineering</li>';
+                                            }
+                                            if($etc >= $_SESSION['jee_adv']) {
+                                                echo '<li>Electrical Engineering</li>';
+                                            }
+                                            if($ece >= $_SESSION['jee_adv']) {
+                                                echo '<li>Electronics Engineering</li>';
+                                            }
+                                            if($che >= $_SESSION['jee_adv']) {
+                                                echo '<li>Chemical Engineering</li>';
+                                            }
+                                        echo '</ul>';
+                                        $href = 'college_detail.php?college_id='.$row['id'];
+                                        echo '<a href="'. $href .'" class="btn btn-secondary" style="color:white;">View College</a>';
+                                    echo '</div>
+                                </div>';
+                            }
+                        }
+                    }
+
+                    /* free result set */
+                    $result->free();
+                }
+
+                if($count == 0) {
+                    echo '<p class="card-text text-muted ml-3">Sorry, No colleges found!</p>';
+                }
+            ?>
+        </div>
+
+        <h3 class="mt-5">Based on JEE Main</h3>
+        <div class="row row-cols-1 row-cols-md-2 pt-3">
+            <?php 
+                $query = "SELECT * FROM college";
+                $count = 0;
+                if ($result = $conn->query($query)) {
+
+                    while ($row = $result->fetch_assoc()) {
+                        if($row['mode_of_admission'] == 'JEE Main') {
+                            $cs = $row['computer_engineering']; 
+                            $mech = $row['mechanical_engineering']; 
+                            $etc = $row['electrical_engineering']; 
+                            $ece = $row['electronics_engineering']; 
+                            $che = $row['chemical_engineering'];
+                            
+                            if(max($cs, $mech, $etc, $ece, $che) >= $_SESSION['jee_main']) {
+                                $count += 1;
+                                echo '<div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                                    <div class="card-header">Recommended</div>
+                                    <div class="card-body">';
+                                        echo '<h5 class="card-title">'.$row['name'].'</h5>';
+                                        echo '<h6 class="card-title mt-3">Available branches</h6>';
+                                        echo '<ul>';
+                                            if($cs >= $_SESSION['jee_main']) {
+                                                echo '<li>Computer Engineering</li>';
+                                            }
+                                            if($mech >= $_SESSION['jee_main']) {
+                                                echo '<li>Mechanical Engineering</li>';
+                                            }
+                                            if($etc >= $_SESSION['jee_main']) {
+                                                echo '<li>Electrical Engineering</li>';
+                                            }
+                                            if($ece >= $_SESSION['jee_main']) {
+                                                echo '<li>Electronics Engineering</li>';
+                                            }
+                                            if($che >= $_SESSION['jee_main']) {
+                                                echo '<li>Chemical Engineering</li>';
+                                            }
+                                        echo '</ul>';
+                                        $href = 'college_detail.php?college_id='.$row['id'];
+                                        echo '<a href="'. $href .'" class="btn btn-secondary" style="color:white;">View College</a>';
+                                    echo '</div>
+                                </div>';
+                                
+                            }
+                            
+                        }
+                    }
+
+                    /* free result set */
+                    $result->free();
+                }
+
+                if($count == 0) {
+                    echo '<p class="card-text text-muted ml-3">Sorry, No colleges found!</p>';
+                }
+            ?>
+        </div>
+
+        <h3 class="mt-5">Based on MHT-CET</h3>
+        <div class="row row-cols-1 row-cols-md-2 pt-3">
+            <?php 
+                $query = "SELECT * FROM college";
+                $count = 0;
+                if ($result = $conn->query($query)) {
+
+                    while ($row = $result->fetch_assoc()) {
+                        if($row['mode_of_admission'] == 'MHT-CET') {
+                            $cs = $row['computer_engineering']; 
+                            $mech = $row['mechanical_engineering']; 
+                            $etc = $row['electrical_engineering']; 
+                            $ece = $row['electronics_engineering']; 
+                            $che = $row['chemical_engineering'];
+
+                            if(max($cs, $mech, $etc, $ece, $che) >= $_SESSION['cet']) {
+                                $count += 1;
+                                echo '<div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+                                    <div class="card-header">Recommended</div>
+                                    <div class="card-body">';
+                                        echo '<h5 class="card-title">'.$row['name'].'</h5>';
+                                        echo '<h6 class="card-title mt-3">Available branches</h6>';
+                                        echo '<ul>';
+                                            if($cs >= $_SESSION['cet']) {
+                                                echo '<li>Computer Engineering</li>';
+                                            }
+                                            if($mech >= $_SESSION['cet']) {
+                                                echo '<li>Mechanical Engineering</li>';
+                                            }
+                                            if($etc >= $_SESSION['cet']) {
+                                                echo '<li>Electrical Engineering</li>';
+                                            }
+                                            if($ece >= $_SESSION['cet']) {
+                                                echo '<li>Electronics Engineering</li>';
+                                            }
+                                            if($che >= $_SESSION['cet']) {
+                                                echo '<li>Chemical Engineering</li>';
+                                            }
+                                        echo '</ul>';
+                                        $href = 'college_detail.php?college_id='.$row['id'];
+                                        echo '<a href="'. $href .'" class="btn btn-secondary" style="color:white;">View College</a>';
+                                    echo '</div>
+                                </div>';
+                            }
+                        }
+                    }
+
+                    /* free result set */
+                    $result->free();
+                }
+
+                if($count == 0) {
+                    echo '<p class="card-text text-muted ml-3">Sorry, No colleges found!</p>';
+                }
+            ?>
+        </div>
+
+    </div>
+    
+
     <?php include 'const/bootstrap_script.php'; ?>
 
 </body>
